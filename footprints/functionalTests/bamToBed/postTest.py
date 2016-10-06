5ae7d512f3e3b4bb158218373e48b2dae7cfc9e9
dauss75 <galaxy@bdds.globusgenomics.org>
2016-10-06 22:37:28 +0000

    clean up

--- footprints/functionalTests/bamToBed/postTest.py
+++ footprints/functionalTests/bamToBed/postTest.py
@@ -6,7 +6,6 @@ if(len(sys.argv) !=2):
     exit(1)
 
 filename = sys.argv[1]
-print('filename:%s' % filename)
 assert(os.path.isfile(filename))
 tbl = read_csv(filename, sep="\t")
 #assert(tbl.shape == (1279016, 6))
@@ -15,5 +14,4 @@ assert(min(tbl.iloc[:, 1].tolist()) == 60015)
 assert(max(tbl.iloc[:, 1].tolist()) == 58607530)
 chromosomes = unique(tbl.iloc[:5, 0]).tolist()
 assert(len(chromosomes) == 1)
-print(chromosomes[0])
 assert(chromosomes[0] == 'chr19')
