d714f1545c3487aa1c8b6d7afec8bcaa086292ed
Ben Heavner <bheavner@gmail.com>
2016-10-06 15:20:16 -0700

    fseq tests

--- footprints/functionalTests/fseq/postTest.py
+++ footprints/functionalTests/fseq/postTest.py
@@ -8,9 +8,9 @@ if(len(sys.argv) !=2):
 filename = sys.argv[1]
 assert(os.path.isfile(filename))
 tbl = read_csv(filename, sep="\t")
-assert(tbl.shape == (1279016, 6))
-assert(min(tbl.iloc[:, 1].tolist()) == 60015)
-assert(max(tbl.iloc[:, 1].tolist()) == 58607534)
+assert(tbl.shape == (17572, 5))
+assert(min(tbl.iloc[:, 1].tolist()) == 64588)
+assert(max(tbl.iloc[:, 1].tolist()) == 58606418)
 chromosomes = unique(tbl.iloc[:5, 0]).tolist()
 assert(len(chromosomes) == 1)
 assert(chromosomes[0] == "chr19")
