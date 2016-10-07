f66685115dcdc1a46782b79f298be17ea8ae5be9
Ben Heavner <bheavner@gmail.com>
2016-10-07 14:56:11 -0700

    changing fseq results...

--- footprints/functionalTests/transform/postTest.py
+++ footprints/functionalTests/transform/postTest.py
@@ -8,7 +8,8 @@ if(len(sys.argv) !=2):
 filename = sys.argv[1]
 assert(os.path.isfile(filename))
 tbl = read_csv(filename, sep="\t")
-assert(tbl.shape == (2970, 3))
+#assert(tbl.shape == (2970, 3))
+assert(tbl.shape == (2976, 3))
 assert(min(tbl.iloc[:, 1].tolist()) == 69929)
 assert(max(tbl.iloc[:, 1].tolist()) == 58577629)
 chromosomes = unique(tbl.iloc[:5, 0]).tolist()
