35967c252665831ea5712f0b7dacf6214fa73244
Ben Heavner <bheavner@systemsbiology.org>
2016-10-21 15:38:05 -0700

    separating fill.R to different files

--- footprints/testdb/src/DBFunctions.R
+++ footprints/testdb/src/DBFunctions.R
@@ -1,51 +1,88 @@
-library(RPostgreSQL)
-library(RUnit)
-
-printf <- function(...) print(noquote(sprintf(...)))
-
 #-------------------------------------------------------------------------------
-runTests <- function()
-{
-  test.getDBConnection()
-} # runTests
-
-#-------------------------------------------------------------------------------
-
 getDBConnection <- function(driver = dbDriver("PostgreSQL"), user= "trenatest", 
                             password="trenatest", dbname="trenatest", 
                             host="whovian") {
   dbConnect(driver, user=user, password=password, dbname=dbname, host=host)
 } # getDBConnection
-
 #-------------------------------------------------------------------------------
-test.getDBConnection <- function()
+region.schema <- function()
 {
-  printf("--- test.getDBConnection")
+  c("loc", "chrom", "start", "endpos")
   
-  db.testConnection <- dbConnect(PostgreSQL(), user= "trenatest", password="trenatest", dbname="trenatest", host="whovian")
+} # region.schema
+#-------------------------------------------------------------------------------
+hit.schema <- function()
+{
+  c("loc", "type", "name", "length", 
+    "strand", "sample_id", "method", "provenance",
+    "score1", "score2", "score3", "score4", "score5", "score6")
   
-  # test the class of db.testConnection
-  checkEquals(class(db.testConnection)[1], "PostgreSQLConnection")
+} # hit.schema
+#-------------------------------------------------------------------------------
+databaseSummary <- function()
+{
+  region.count <- dbGetQuery(db.wellington, "select count(*) from regions")[1,1]
+  hit.count <- dbGetQuery(db.wellington, "select count(*) from hits")[1,1]
+  printf("%d hits in %d regions", hit.count, region.count)
+  
+} # databaseSummary
+#-------------------------------------------------------------------------------
+## NOTE: original sql command started with \connect wholeBrain-wellington - needed?
+createEmptyDatabaseTables <- function(dbuser, dbConnection)
+{
+  sql_command <- '
+  drop table regions;
+  drop table hits;
+  
+  create table regions(loc varchar primary key,
+  chrom varchar,
+  start int,
+  endpos int);
+  
+  grant all on table "regions" to trena;
+  
+  create table hits(loc varchar,
+  type varchar,
+  name varchar,
+  length int,
+  strand char(1),
+  sample_id varchar,
+  method varchar,
+  provenance varchar,
+  score1 real,
+  score2 real,
+  score3 real,
+  score4 real,
+  score5 real,
+  score6 real);
+  
+  grant all on table "hits" to ' + dbuser + ';'
 
-  # test that we can read tables from db.testConnection
-  checkEquals(dbExistsTable(db.testConnection, "testtable"), TRUE)
+  dbGetQuery(dbConnection, sql_command)
+} # createEmptyDatabaseTables
+#-------------------------------------------------------------------------------
+## NOTE: original sql command started with \connect wholeBrain-wellington - needed?
+appendToRegionsTable <- function(tbl, dbConnection)
+{
+  write.table(tbl, file="regions.tsv", row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t", na="NULL")
+  sql_command <- "
+  \copy regions from 'regions.tsv' delimiter E'\t' CSV NULL as 'NULL';"
+  dbGetQuery(dbConnection, sql_command)
   
-  # test that we can write to db.testConnection
-  checkEquals(
-    dbWriteTable(conn = db.testConnection,
-               name = "testtable",
-               value=data.frame("teststring"), 
-               append = TRUE, 
-               row.names = FALSE),
-    TRUE)
+} # appendToRegionsTable
+#-------------------------------------------------------------------------------
+## NOTE: original sql command started with \connect wholeBrain-wellington - needed?
+appendToHitsTable <- function(tbl, dbConnection)
+{
+  write.table(tbl, file="hits.tsv", row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t", na="NULL")
+  sql_command <- "
+  \copy hits from 'hits.tsv' delimiterE'\t' CSV NULL as 'NULL'"
+  dbGetQuery(dbConnection, sql_command)
   
-  # test that we can read from db.testConnection
-  checkEquals(
-    is.data.frame(
-      dbGetQuery(db.testConnection, "select * from testtable")
-    ), TRUE)
-  
-  # clean up and disconnect
-  dbGetQuery(db.testConnection, "DELETE FROM testtable")
-  dbDisconnect(db.testConnection)
-} # test.getDBConnection
+} # appendToHitsTable
+#-------------------------------------------------------------------------------
+fill.to.database <- function(tbl.regions, tbl.hits, dbConnection)
+{
+  appendToRegionsTable(tbl.regions, dbConnection)
+  appendToHitsTable(tbl.hits, dbConnection)
+}
--- footprints/testdb/wellington/fill.R
+++ footprints/testdb/wellington/fill.R
@@ -1,12 +1,10 @@
-library(RPostgreSQL)
-library(GenomicRanges)
-library(RUnit)
-source("../DB_initialization/regionAndHitsSchemas.R")
-source("../src/getDBConnection.R")
-
-printf <- function(...) print(noquote(sprintf(...)))
+source("../src/dependencies.R")
+source("../src/DBFunctions.R")
+source("../src/TableParsing.R")
+source("../src/Tests.R")
 
 #-------------------------------------------------------------------------------
+# path to output of the makefile based tests for wellington on CHR 19
 wellington.path <- "../../functionalTests/output/wellington"
 test.sampleID <- "ENCSR000DBY"
 
@@ -21,219 +19,8 @@ if(!exists("db.fimo"))
                               host="whovian")
 
 knownLocs <- new.env(parent=emptyenv())
-
 #-------------------------------------------------------------------------------
-runTests <- function()
-{
-  test.readWellingtonTable()
-  test.mergeFootprintsWithFimo()
-  test.splitTableIntoRegionsAndWellingtonHits()
-  #test.fill.to.database()
-    
-  #test.combineFootprintsAndFimo()
-  #test.combineFootprintsAndDatabasedFimo()
-
-} # runTests
-#-------------------------------------------------------------------------------
-readWellingtonTable <- function(directory, sampleID, nrows=NA, chromosome=NA)
-{
-   filename <- grep(sampleID, list.files(directory), v=TRUE)
-   full.path <- file.path(directory, filename)
-
-   if(!file.exists(full.path))
-       return(data.frame)
-   
-   tbl <- read.table(full.path, sep="\t", as.is=TRUE)
-   colnames(tbl) <- c("chrom", "start", "end", "name", "score", "strand")
-   #tbl$chrom <- paste("chr", tbl$chrom, sep="")
-   if(!is.na(chromosome))
-      tbl <- subset(tbl, chrom==chromosome)
-
-   if(!is.na(nrows))
-      tbl <- tbl[1:nrows,]
-
-   invisible(tbl)
-
-} # readWellingtonTable
-#-------------------------------------------------------------------------------
-test.readWellingtonTable <- function()
-{
-   printf("--- test.readWellingtonTable")
-
-   tbl <- readWellingtonTable(wellington.path, test.sampleID, 5, "chr19")
-   checkEquals(dim(tbl), c(5,6))
-   checkEquals(colnames(tbl), c("chrom", "start", "end", "name", "score", "strand"))
-   checkEquals(unique(tbl$chrom), "chr19")
-
-     # now read without chrom or nrow constraints
-   tbl <- readWellingtonTable(wellington.path, test.sampleID)
-   checkEquals(ncol(tbl), 6)
-   checkTrue(nrow(tbl) > 300)
-   checkEquals(colnames(tbl), c("chrom", "start", "end", "name", "score", "strand"))
-   checkEquals(head(sort(unique(tbl$chrom))), "chr19")
-   
-} # test.readWellingtonTable
-#-------------------------------------------------------------------------------
-mergeFimoWithFootprints <- function(tbl.fp, sampleID)
-{
-  chromosome <- unique(tbl.fp$chrom)
-    # enforce treatment of just one chromosome at a time
-  stopifnot(length(chromosome) == 1)
-  min.pos <- min(tbl.fp$start)
-  max.pos <- max(tbl.fp$end)
-  
-  fimo.chromosome <- sub("chr", "", chromosome)
-  query <- sprintf("select * from hg38 where chr='%s' and start >= %d and endpos <= %d",
-                   fimo.chromosome, min.pos, max.pos)
-
-  tbl.fimo <- dbGetQuery(db.fimo, query)
-  colnames(tbl.fimo) <- c("motif", "chrom", "motif.start", "motif.end", "motif.strand", "fimo.score",
-                          "fimo.pvalue", "empty", "motif.sequence")
-  tbl.fimo <- tbl.fimo[, -grep("empty", colnames(tbl.fimo))]
-  tbl.fimo$chrom <- paste("chr", tbl.fimo$chrom, sep="")
-
-  gr.fimo <- with(tbl.fimo, GRanges(seqnames=chrom, IRanges(start=motif.start, end=motif.end)))
-
-    # --- get some footprints
-
-  gr.wellington <- with(tbl.fp,   GRanges(seqnames=chrom, IRanges(start=start, end=end)))
-  tbl.overlaps <- as.data.frame(findOverlaps(gr.fimo, gr.wellington, type="within"))
-
-  tbl.fimo$loc <- with(tbl.fimo, sprintf("%s:%d-%d", chrom, motif.start, motif.end))
-  tbl.fimo$method <- "WELLINGTON"
-  tbl.fimo$sample_id <- sampleID
-  tbl.regions <- tbl.fimo[tbl.overlaps$queryHits,]
-
-  tbl.regions <- cbind(tbl.regions, wellington.score=tbl.fp[tbl.overlaps$subjectHits, "score"])
-  invisible(tbl.regions)
-
-} # mergeFimoWithFootprints
-#-------------------------------------------------------------------------------
-test.mergeFootprintsWithFimo <- function()
-{
-   printf("--- test.mergeFootprintsWithFimo")
-   tbl.fp <- readWellingtonTable(wellington.path, test.sampleID, nrow=3, "chr19")
-   tbl <- mergeFimoWithFootprints(tbl.fp, test.sampleID)
-   checkEquals(ncol(tbl), 12)
-   checkEquals(sort(colnames(tbl)),
-               c("chrom", "fimo.pvalue", "fimo.score", "loc", "method",
-                 "motif", "motif.end", "motif.sequence", "motif.start", "motif.strand", "sample_id",
-                 "wellington.score"))
-   checkTrue(nrow(tbl) >= 8)
-
-   duplicated.loc <- "chr19:636760-636770"
-   checkEquals(length(grep(duplicated.loc, tbl$loc)), 5)
-     #  3 distinct motifs mapped to this region
-   checkEquals(sort(subset(tbl, loc == duplicated.loc)$motif),
-               c("MA0003.3", "MA0812.1", "MA0812.1", "MA0814.1", "MA0814.1"))
-   
-   invisible(tbl)
-
-} # test.mergeFootprintsWithFimo
-#-------------------------------------------------------------------------------
-splitTableIntoRegionsAndWellingtonHits <- function(tbl, minid)
-{
-   tbl.regions <- unique(tbl[, c("loc", "chrom", "motif.start", "motif.end")])
-   colnames(tbl.regions) <- region.schema() # 29
-    # c("loc", "chrom", "motif_start", "motif_end")
-
-   new.locs <- setdiff(tbl.regions$loc, names(knownLocs))
-     # enter these new.locs into the hash
-   lapply(new.locs, function(loc) knownLocs[[loc]] <- 0)   
-   printf("novel locs: %d new/%d possible (%d now known, %d dups)",
-          length(new.locs), nrow(tbl.regions), length(names(knownLocs)), nrow(tbl.regions) - length(new.locs))
-
-   if(length(new.locs) > 0){
-      keepers <- match(new.locs, tbl.regions$loc)
-      tbl.regions <- tbl.regions[keepers,]
-    } else {
-       tbl.regions <- data.frame()
-       }
-
-   tbl.hits <- tbl[, c("loc", "motif", "motif.strand", "sample_id", "method", "wellington.score",
-                       "fimo.score", "fimo.pvalue")]
-   tbl.hits$length <- with(tbl, 1 + motif.end - motif.start)
-   tbl.hits$provenance <- minid
-   tbl.hits$score4 <- NA
-   tbl.hits$score5 <- NA
-   tbl.hits$score6 <- NA
-   tbl.hits$type <- "motif.in.footprint"
-   coi <- c("loc", "type", "motif", "length", "motif.strand", "sample_id", "method", "provenance", 
-            "wellington.score", "fimo.score", "fimo.pvalue", "score4", "score5", "score6")
-   tbl.hits <- tbl.hits[, coi]
-   colnames(tbl.hits) <- hit.schema()
-   invisible(list(regions=tbl.regions, hits=tbl.hits))
-    
-} # splitTableIntoRegionsAndWellingtonHits    
-#-------------------------------------------------------------------------------
-test.splitTableIntoRegionsAndWellingtonHits <- function(tbl)
-{
-   printf("--- test.splitTableIntoRegionsAndWellingtonHits")
-   tbl.fp <- readWellingtonTable(wellington.path, test.sampleID, nrow=3, "chr19")
-   tbl <- mergeFimoWithFootprints(tbl.fp, test.sampleID)
-
-   x <- splitTableIntoRegionsAndWellingtonHits(tbl, "minid")
-   checkEquals(sort(names(x)), c("hits", "regions"))
-   checkEquals(colnames(x$regions), region.schema()) #
-   checkEquals(colnames(x$hits), hit.schema())
-
-} # test.splitTableIntoRegionsAndWellingtonHits    
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
-test.fill.to.database <- function()
-{
-   printf("--- test.fill.to.database")
-   createEmptyDatabaseTables()
-   knownLocs <<- new.env(parent=emptyenv())
-
-   tbl.fp <- readWellingtonTable(wellington.path, test.sampleID, nrow=3, "chr21")
-   tbl <- mergeFimoWithFootprints(tbl.fp, test.sampleID)
-   x <- splitTableIntoRegionsAndWellingtonHits(tbl, "minid")
-
-   fill.to.database(x$regions, x$hits)
-   checkEquals(sort(dbListTables(db.wellington)), c("hits", "regions"))
-
-   tbl.regions <- dbGetQuery(db.wellington, "select * from regions")
-   checkEquals(dim(tbl.regions), c(2, 4))
-   checkTrue(all(region.schema() == colnames(tbl.regions)))
-
-   tbl.hits <- dbGetQuery(db.wellington, "select * from hits")
-   checkTrue(all(hit.schema() == colnames(tbl.hits)))
-   checkEquals(dim(tbl.hits), c(8, 14))
-   checkEquals(unique(tbl.hits$type), "motif.in.footprint")
-   checkTrue(all(tbl.hits$loc %in% tbl.regions$loc))
-
-} # test.fill.to.database
-#-------------------------------------------------------------------------------
-databaseSummary <- function()
-{
-    region.count <- dbGetQuery(db.wellington, "select count(*) from regions")[1,1]
-    hit.count <- dbGetQuery(db.wellington, "select count(*) from hits")[1,1]
-    printf("%d hits in %d regions", hit.count, region.count)
-
-} # databaseSummary
-#-------------------------------------------------------------------------------
-createEmptyDatabaseTables <- function()
-{
-   system("/bin/psql -f createTables.sql")
-    
-} # appendToRegionsTable
-#-------------------------------------------------------------------------------
-appendToRegionsTable <- function(tbl)
-{
-   write.table(tbl, file="regions.tsv", row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t", na="NULL")
-   system("/bin/psql -f fillRegions.sql")
-    
-} # appendToRegionsTable
-#-------------------------------------------------------------------------------
-appendToHitsTable <- function(tbl)
-{
-   write.table(tbl, file="hits.tsv", row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t", na="NULL")
-   system("/bin/psql -f fillHits.sql")
-
-} # appendToHitsTable
-#-------------------------------------------------------------------------------
+#TODO: fix fill.to.database call
 fill.all.samples.by.chromosome <- function(chromosome)
 {
    knownLocs <<- new.env(parent=emptyenv())
@@ -274,21 +61,9 @@ examine.region <- function(chromosome, start, end, sampleIDs)
 
 } # examine.region
 #-------------------------------------------------------------------------------
-test_examine.region <- function()
-{
-   printf("--- test_examine.region")
-   sampleIDs <- c("ENCSR000EJB")
-   chrom <- "chr19"
-   start <- 44903773
-   end <- 44903785
-   examine.region(chrom, start, end, sampleIDs)
-
-} # test_examine.region
-#-------------------------------------------------------------------------------
 if(!interactive()){
     chromosomes <- paste("chr", c(1:18, 20:22), sep="")
     #chromosomes <- paste("chr", c(19), sep="")
     for(chromosome in chromosomes)
         fill.all.samples.by.chromosome(chromosome)
     }
-
