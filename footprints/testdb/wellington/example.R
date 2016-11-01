34c457dc940889c562078709ed55587a43341a46
Ben Heavner <bheavner@systemsbiology.org>
2016-11-01 10:13:37 -0700

    updating tests and example

--- footprints/testdb/wellington/example.R
+++ footprints/testdb/wellington/example.R
@@ -1,8 +1,8 @@
-#-------------------------------------------------------------------------------
 # users can either step through this file, or call this file with 
 # r -f example.R
-#
 
+#-------------------------------------------------------------------------------
+# load functions and dependencies
 source("../src/dependencies.R")
 source("../src/dbFunctions.R")
 source("../src/tableParsing.R")
@@ -10,8 +10,9 @@ source("../src/tests.R")
 source("../src/main.R")
 
 #-------------------------------------------------------------------------------
-# path to output of the makefile based tests for wellington on CHR 19
-# on whovian, use this:
+# set path to wellington output 
+
+# for makefile based tests for wellington on CHR 19 on whovian, use this:
 wellington.path <- "/local/Ben/BDDS/footprints/functionalTests/output/wellington"
 test.sampleID <- "ENCSR000DBY"
 
@@ -35,12 +36,7 @@ if(!exists("db.wellington"))
 
 if(!exists("db.fimo"))
   db.fimo <- getDBConnection("fimo")
-
-#knownLocs <- new.env(parent=emptyenv())
-
 #-------------------------------------------------------------------------------
-# can either step through this file, or call this file with 
-# r -f example.R
 
 if(!interactive()){
     #chromosomes <- paste("chr", c(1:18, 20:22), sep="")
