172a929f2076b8c04fc54afe21bfcf42a289eda1
marichards <marichards88@gmail.com>
2017-08-09 11:07:17 -0700

    Added all tissues to dbFunctions list

--- footprints/testdb/src/dbFunctions.R
+++ footprints/testdb/src/dbFunctions.R
@@ -9,167 +9,347 @@ getDBConnection <- function(database) {
     dbname="trenatest" 
     host="bdds-rds.globusgenomics.org"
 
-  } else if (database == "test_piq_localhost") {      
-    user = "trena"
-    password = "trena"
-    dbname = "test_piq"
-    host = "localhost"
-
-  } else if (database == "test_brain_wellington_localhost") {
-    user = "trena"
-    password = "trena"
-    dbname = "test_brain_wellington"
-    host = "localhost"
+  } else if (database == "fimo_localhost") {
+    user= "trena" 
+    password="trena" 
+    dbname="fimo"
+    host="localhost"
     
-  } else if (database == "test_brain_hint_localhost") {
-    user = "trena"
-    password = "trena"
-    dbname = "test_brain_hint"
-    host = "localhost"
- 
-  } else if (database == "brain_wellington_20_localhost") {
-    user = "trena"
-    password = "trena"
-    dbname = "brain_wellington_20"
-    host = "localhost"
-    
-  } else if (database == "brain_hint_20_localhost") {
-    user = "trena"
-    password = "trena"
-    dbname = "brain_hint_20"
-    host = "localhost"
-
   } else if (database == "adrenal_gland_wellington_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "adrenal_gland_wellington_20"
-    host = "localhost"
-    
+    host = "localhost"    
   } else if (database == "adrenal_gland_hint_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "adrenal_gland_hint_20"
     host = "localhost"
-
+    
   } else if (database == "bone_element_wellington_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "bone_element_wellington_20"
-    host = "localhost"
-    
+    host = "localhost"    
   } else if (database == "bone_element_hint_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "bone_element_hint_20"
     host = "localhost"
 
+  } else if (database == "brain_wellington_16_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "brain_wellington_16"
+    host = "localhost"    
+  } else if (database == "brain_hint_16_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "brain_hint_16"
+    host = "localhost"    
+    
+  } else if (database == "brain_wellington_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "brain_wellington_20"
+    host = "localhost"    
+  } else if (database == "brain_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "brain_hint_20"
+    host = "localhost"
+    
   } else if (database == "bronchus_wellington_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "bronchus_wellington_20"
-    host = "localhost"
-    
+    host = "localhost"    
   } else if (database == "bronchus_hint_20_localhost") {
     user = "trena"
     password = "trena"
     dbname = "bronchus_hint_20"
     host = "localhost"
 
-} else if (database == "esophagus_wellington_20_localhost") {
+  } else if (database == "esophagus_wellington_20_localhost") {      
     user = "trena"
     password = "trena"
     dbname = "esophagus_wellington_20"
     host = "localhost"
-    
-  } else if (database == "esophagus_hint_20_localhost") {
+  } else if (database == "esophagus_hint_20_localhost") {      
     user = "trena"
     password = "trena"
     dbname = "esophagus_hint_20"
     host = "localhost"
-    
-  } else if (database == "skin_wellington_20_localhost") {
+
+  } else if (database == "extraembryonic_structure_hint_20_localhost") {      
     user = "trena"
     password = "trena"
-    dbname = "skin_wellington_20"
+    dbname = "extraembryonic_structure_hint_20"
+    host = "localhost"
+  } else if (database == "extraembryonic_structure_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "extraembryonic_structure_wellington_20"
     host = "localhost"
     
-  } else if (database == "skin_hint_20_localhost") {
+  } else if (database == "eye_hint_20_localhost") {
     user = "trena"
     password = "trena"
-    dbname = "skin_hint_20"
+    dbname = "eye_hint_20"
+    host = "localhost"
+  } else if (database == "eye_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "eye_wellington_20"
     host = "localhost"
     
-  } else if (database == "lymphoblast_wellington_20_localhost") {
+  } else if (database == "gonad_hint_20_localhost") {
     user = "trena"
     password = "trena"
-    dbname = "lymphoblast_wellington_20"
+    dbname = "gonad_hint_20"
+    host = "localhost"
+  } else if (database == "gonad_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "gonad_wellington_20"
     host = "localhost"
     
-  } else if (database == "lymphoblast_hint_20_localhost") {
+  } else if (database == "heart_hint_20_localhost") {      
     user = "trena"
     password = "trena"
-    dbname = "lymphoblast_hint_20"
-    host = "localhost"    
- 
-  } else if (database == "brain_wellington_16_localhost") {
+    dbname = "heart_hint_20"
+    host = "localhost"
+  } else if (database == "heart_wellington_20_localhost") {      
     user = "trena"
     password = "trena"
-    dbname = "brain_wellington_16"
+    dbname = "heart_wellington_20"
     host = "localhost"
     
-  } else if (database == "brain_hint_16_localhost") {
+  } else if (database == "kidney_hint_20_localhost") {
     user = "trena"
     password = "trena"
-    dbname = "brain_hint_16"
+    dbname = "kidney_hint_20"
     host = "localhost"
-    
-  } else if (database == "skin_wellington_16_localhost") {
+  } else if (database == "kidney_wellington_20_localhost") {      
     user = "trena"
     password = "trena"
-    dbname = "skin_wellington_16"
+    dbname = "kidney_wellington_20"
     host = "localhost"
     
-  } else if (database == "skin_hint_16_localhost") {
+  } else if (database == "large_intestine_hint_20_localhost") {
     user = "trena"
     password = "trena"
-    dbname = "skin_hint_16"
+    dbname = "large_intestine_hint_20"
+    host = "localhost"
+  } else if (database == "large_intestine_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "large_intestine_wellington_20"
     host = "localhost"
     
-  } else if (database == "lymphoblast_wellington_16_localhost") {
+  } else if (database == "liver_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "liver_hint_20"
+    host = "localhost"
+  } else if (database == "liver_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "liver_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "lung_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "lung_hint_20"
+    host = "localhost"
+  } else if (database == "lung_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "lung_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "lymphatic_vessel_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "lymphatic_vessel_hint_20"
+    host = "localhost"
+  } else if (database == "lymphatic_vessel_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "lymphatic_vessel_wellington_20"
+    host = "localhost"
+
+  } else if (database == "lymphoblast_wellington_16_localhost") {      
     user = "trena"
     password = "trena"
     dbname = "lymphoblast_wellington_16"
-    host = "localhost"
-    
+    host = "localhost"    
   } else if (database == "lymphoblast_hint_16_localhost") {
     user = "trena"
     password = "trena"
     dbname = "lymphoblast_hint_16"
     host = "localhost"    
     
+  } else if (database == "lymphoblast_wellington_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "lymphoblast_wellington_20"
+    host = "localhost"    
+  } else if (database == "lymphoblast_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "lymphoblast_hint_20"
+    host = "localhost"    
+    
+  } else if (database == "mammary_gland_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "mammary_gland_hint_20"
+    host = "localhost"
+  } else if (database == "mammary_gland_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "mammary_gland_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "mouth_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "mouth_hint_20"
+    host = "localhost"
+  } else if (database == "mouth_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "mouth_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "muscle_organ_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "muscle_organ_hint_20"
+    host = "localhost"
+  } else if (database == "muscle_organ_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "muscle_organ_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "pancreas_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "pancreas_hint_20"
+    host = "localhost"
+  } else if (database == "pancreas_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "pancreas_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "prostate_gland_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "prostate_gland_hint_20"
+    host = "localhost"
+  } else if (database == "prostate_gland_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "prostate_gland_wellington_20"
+    host = "localhost"
+
+  } else if (database == "skin_wellington_16_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "skin_wellington_16"
+    host = "localhost"    
+  } else if (database == "skin_hint_16_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "skin_hint_16"
+    host = "localhost"
+
+  } else if (database == "skin_wellington_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "skin_wellington_20"
+    host = "localhost"        
+  } else if (database == "skin_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "skin_hint_20"
+    host = "localhost"
+    
+  } else if (database == "spinal_cord_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "spinal_cord_hint_20"
+    host = "localhost"
+  } else if (database == "spinal_cord_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "spinal_cord_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "stomach_hint_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "stomach_hint_20"
+    host = "localhost"
+  } else if (database == "stomach_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "stomach_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "thyroid_gland_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "thyroid_gland_hint_20"
+    host = "localhost"
+  } else if (database == "thyroid_gland_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "thyroid_gland_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "tongue_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "tongue_hint_20"
+    host = "localhost"
+  } else if (database == "tongue_wellington_20_localhost") {      
+    user = "trena"
+    password = "trena"
+    dbname = "tongue_wellington_20"
+    host = "localhost"
+    
+  } else if (database == "urinary_bladder_hint_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "urinary_bladder_hint_20"
+    host = "localhost"    
+  } else if (database == "urinary_bladder_wellington_20_localhost") {
+    user = "trena"
+    password = "trena"
+    dbname = "urinary_bladder_wellington_20"
+    host = "localhost"
+ 
   } else if (database == "test_wellington_localhost") {
     user = "trena"
     password = "trena"
     dbname = "testwellington"
-    host = "localhost"
-    
+    host = "localhost"    
   } else if (database == "test_hint_localhost") {
     user = "trena"
     password = "trena"
     dbname = "testhint"
-    host = "localhost"
-
-  } else if (database == "fimo_localhost") {
-    user= "trena" 
-    password="trena" 
-    dbname="fimo"
-    host="localhost"
-
-  } else if (database == "wellington_whovian") {
+    host = "localhost"    
+  } else if (database == "test_piq_localhost") {      
     user = "trena"
     password = "trena"
-    dbname = "wholeBrain-wellington"
-    host = "whovian"
+    dbname = "test_piq"
+    host = "localhost"
+
   }
   
   dbConnect(drv=driver, user=user, password=password, dbname=dbname, host=host, 
